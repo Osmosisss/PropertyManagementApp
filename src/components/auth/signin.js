92264009908d02a7c7ab9bb876e3babc89b773d6
Osmosisss <ozyosmosis@gmail.com>
2018-10-02 13:12:57 -0600

    setup redux form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class Signin extends Component {
+
+    renderInput(field) {
+        return <input className="form-control" {...field.input} type="field.type" />
+    }
+
+    handleFormSubmit = function () {
+        console.log('check check');
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
         return (
-            <div>
-                sign in from
-            </div>
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <label htmlFor="email">Email</label>
+                <Field name="email" component={this.renderInput} type="text" />
+                <label htmlFor="password">Password</label>
+                <Field name="password" component={this.renderInput} type="password" />
+
+                <button action="submit" className="btn btn-primary">Sign in</button>
+            </form>
         );
     }
 }
 
-export default Signin;
+export default reduxForm({ form: "signin" })(Signin);
