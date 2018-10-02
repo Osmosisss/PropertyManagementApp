7088844b68784f06c22dcb292cc4f79d22459ac6
Osmosisss <ozyosmosis@gmail.com>
2018-10-02 12:24:58 -0600

    setup project and header HOC

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/app';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/header";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './style/main.scss';
-
+import "bootstrap/dist/css/bootstrap.css";
+import "./style/main.scss";
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Header>
+
+        </Header>
       </BrowserRouter>
-    </Provider>
-    , document.querySelector('.app-wrapper'));
+    </Provider>,
+    document.querySelector(".app-wrapper")
+  );
 }
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
