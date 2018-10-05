45d7bcf7e63301d79bd19d1ff17a86847e443c65
Osmosisss <ozyosmosis@gmail.com>
2018-10-04 18:53:25 -0600

    setup action and get request to receive newletter archive

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
+
+import { FETCH_NEWSLETTER_ARCHIVE } from './types';
+
 import axios from 'axios';
 import history from '../history';
+import rootReducer from '../reducers';
 
 const ROOT_URL = 'https://bottega-property-management.herokuapp.com';
 
@@ -14,6 +18,20 @@ export function signinUser({ email, password }) {
             })
             .catch(error => {
                 console.log(error);
+            })
+    }
+}
+
+export function fetchNewsletterArchive() {
+    return function (dispatch) {
+        axios.get(`${ROOT_URL}/newsletterArchive`, {
+            headers: { authorization: localStorage.getItem('token') }
+        })
+            .then(response => {
+                dispatch({
+                    type: FETCH_NEWSLETTER_ARCHIVE,
+                    payload: response.data
+                })
             })
     }
 }
