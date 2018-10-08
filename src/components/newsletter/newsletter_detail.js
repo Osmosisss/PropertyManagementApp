713c63a429f104c5f851111301b7943ab0501408
Osmosisss <ozyosmosis@gmail.com>
2018-10-08 15:26:15 -0600

    added newsletter detail component and corresponding route

---
+++ src/components/newsletter/newsletter_detail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewsletterDetail extends Component {
+    render() {
+        return (
+            <div>
+                newsletter details
+            </div>
+        );
+    }
+}
+
+export default NewsletterDetail;
