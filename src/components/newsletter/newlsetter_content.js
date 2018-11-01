17e9df1c65ed86b5cc46392459a0b11ce12c3d3b
Osmosisss <ozyosmosis@gmail.com>
2018-10-31 20:05:45 -0600

    setup newsletter_content.scss

--- src/components/newsletter/newlsetter_content.js
+++ src/components/newsletter/newlsetter_content.js
@@ -6,8 +6,8 @@ class NewsletterContent extends Component {
     renderItem() {
         if (this.props.latestItem) {
             return (
-                <div>
-                    <div>
+                <div className="newsletter-content">
+                    <div className="image-container">
                         <img src={this.props.latestItem.imageUrl} />
                         <h2>{this.props.latestItem.title}</h2>
 
