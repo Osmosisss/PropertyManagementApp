50ac13089c69b3bfe522dd4f4ecdd7969fa6fabb
Osmosisss <ozyosmosis@gmail.com>
2018-11-04 14:46:54 -0700

    added addTitle and addHeaderBorder HOC to newsletter_detail component

--- src/components/newsletter/newsletter_detail.js
+++ src/components/newsletter/newsletter_detail.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
+import addTitle from '../HOC/addTitle';
+import addHeaderBorder from '../HOC/addHeaderBorder';
+
 class NewsletterDetail extends Component {
 
     componentDidMount() {
@@ -45,5 +48,8 @@ function mapStateToProps(state) {
         fetchedItem: state.newsletter.fetchedItem
     }
 }
+
+NewsletterDetail = addTitle(NewsletterDetail, 'Newsletter Archive');
+NewsletterDetail = addHeaderBorder(NewsletterDetail);
 
 export default connect(mapStateToProps, actions)(NewsletterDetail);
